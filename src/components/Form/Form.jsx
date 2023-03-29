09d4f76e5c288ecd9be2525e552cf7a0e753c0cf
DenisTunyk <tundenis43@gmail.com>
2023-03-29 15:22:55 +0300

    finish

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,19 @@
+import { exChange } from 'service/service';
+
 export const Form = () => {
   const handleSubmit = e => {
     e.preventDefault(e.target.value);
-    console.log(e.target.elements.currency.value);
+    const data = e.target.elements.currency.value;
+    const arrData = data.split(' ');
+
+    // 15 USD in UAH
+    const to = arrData[3];
+    const from = arrData[1];
+    const amount = arrData[0];
+
+    exChange(to, from, amount).then(data => console.log(data));
+
+    e.target.reset();
   };
   return (
     <>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ export const Home = () => {
   return (
     <>
       <p>Home</p>
+      <p>Input string for example: 15 USD in UAH</p>
       <Form></Form>;
     </>
   );
